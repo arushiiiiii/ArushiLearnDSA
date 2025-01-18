951115b25caf4504973fec186cd154897c7e7ca5
arushiiiiii <garushi850@gmail.com>
2025-01-18 22:33:43 +0530

    test to commit

--- Lectures/lecture 9/code/c1.java
+++ Lectures/lecture 9/code/c1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class c1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // Find the largest of three numbers
+        // Find the largest of three numbers.
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
